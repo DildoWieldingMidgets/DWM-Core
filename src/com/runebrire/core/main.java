481716007a81c1e53676b94324080dd47797e217
Desty1 <dan_hockey_8@hotmail.com>
2013-08-27 20:31:38 -0600

    initial commit

---
+++ src/com/runebrire/core/main.java
@@ -0,0 +1,16 @@
+package com.runebrire.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
