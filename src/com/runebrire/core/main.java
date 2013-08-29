d4c8440cc48c5f1473d68cc221d6a23acf5f44fe
Desty1 <dan_hockey_8@hotmail.com>
2013-08-29 13:01:40 -0600

    MySql stuff

--- src/com/runebrire/core/main.java
+++ src/com/runebrire/core/main.java
@@ -1,15 +1,21 @@
 package com.runebrire.core;
 
+import java.sql.Connection;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.runebrire.core.data.MySQL;
 
 public class main extends JavaPlugin {
 	/*
 	 * @author Desty
 	 */
+	MySQL MySQL = new MySQL(this, "host.name", "Ttor_database", "3119", "user", "pass");
+	Connection c = null;
 
 	@Override
 	public void onEnable() {
-
+		c = MySQL.openConnection();
 	}
 
 	@Override
